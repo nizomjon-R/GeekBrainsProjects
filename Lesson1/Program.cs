cad8546ea01ee8582a45f3536a0e7349abd5101d
Nizomjon <rahmonberdievnizomjon@gmail.com>
2022-05-03 10:19:58 +0500

    done lesson1

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -10,6 +10,15 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hi GeekBrains it's my first home work!!!");
+            Console.WriteLine("Please inter your first name");
+            string userName = Console.ReadLine();
+            DateTime myDateTame = DateTime.Now;
+            Console.WriteLine($"Hello, {userName} today date time is {myDateTame}");
+            //We can do this type of way
+            Console.WriteLine("Hello, {0} today date time is {1}", userName, myDateTame);
+            Console.WriteLine("Hello, " + userName + " today date time is " + myDateTame);
+            Console.ReadLine();
         }
     }
 }
