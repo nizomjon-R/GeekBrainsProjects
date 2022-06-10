ac41f4737fbfd03f6251c6a8b39a7caca398d427
nizomjon-R <rahmonberdievnizomjon@gmail.com>
2022-06-10 11:45:01 +0500

    Fenished lasson6

---
+++ Lesson6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] proclist = Process.GetProcesses();
+            foreach (Process process in proclist)
+            {
+                Console.WriteLine(process.ProcessName);
+            }
+            foreach (var item in Process.GetProcessesByName("WhatsApp"))
+            {
+               item.Kill();   
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
